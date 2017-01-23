04f1dec706131b1b4ad719fcd337332c94b7c73e
Sheplu <jean.burellier@gmail.com>
2017-01-23 16:17:22 +0100

    update config with test db

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 module.exports = {
-	'secretKey': 'azertyuiop'
+	'secretKey': 'azertyuiop',
+	'development': 'mongodb://localhost:27017/NodeAPI',
+	'test': 'mongodb://localhost:27017/NodeAPITest'
 }
